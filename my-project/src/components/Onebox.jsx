fd754b1a624c6bbc464f2767624b4ca762e7b6cb
Vishnu Jangid <vishnu.j8503@gmail.com>
2024-08-11 13:28:33 +0530

    blankpage,modetoggel

--- my-project/src/components/Onebox.jsx
+++ my-project/src/components/Onebox.jsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
+import axios from 'axios';
 import Sidebar from "./Sidebar";
 import EmailList from "./EmailList";
 import EmailView from "./Emailview";
+import Blankpage from "./Blankpage";
+
 
 function Onebox() {
+  const [mails, setMails] = useState([]);
+  const [selectedTab, setSelectedTab] = useState('blank');
+  const fetchMails = async () => {
+    const token = localStorage.getItem('authToken');
+    console.log(token);
+    try {
+      const response = await axios.get('https://hiring.reachinbox.xyz/api/v1/onebox/list', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setMails(response.data);
+    } catch (error) {
+      console.error('Error fetching mails:', error);
+    }
+  };
+
+  const handleSelect = (tab) => {
+    setSelectedTab(tab);
+    console.log("Inbox");
+    if (tab === 'inbox') {
+      fetchMails();
+    }
+  };
+
+
   return (
-    <div className="Onebox w-full">
-      <div className="flex h-screen w-full bg-gray-100">
-        <Sidebar />
-        <div className=" w-full" >
+    <div className="Onebox w-full dark:bg-black">
+      <div className="flex h-screen w-full dark:text-white bg-gray-100 dark:bg-black">
+        <Sidebar onSelect={handleSelect} />
+        <div className="w-full h-full" >
+         
             <Header/>
-          <div>
-          <EmailList />
+            {selectedTab === 'blank' ? (
+             <Blankpage/>
+        ) : (
+          <div className="w-full h-full">
+          <EmailList emails={mails} />
           <EmailView />
           </div>
+        )}
+           
+            
+         
         </div>
       </div>
     </div>
--- my-project/src/components/Sidebar.jsx
+++ my-project/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { CiHome } from "react-icons/ci";
 import { BsFillSendFill } from "react-icons/bs";
 import { MdOutlinePersonSearch } from "react-icons/md";
 
-const Sidebar = () => {
+const Sidebar = ({onSelect}) => {
   return (
     <div className='w-14 h-full flex-col justify-center items-center dark:bg-zinc-800 dark:text-white shadow-md py-4'>
         <div className='w-7 h-6 m-auto rounded-sm flex justify-center text-xl bg-zinc-800 text-white  dark:bg-white dark:text-gray-900 font-bold' >
@@ -16,7 +16,7 @@ const Sidebar = () => {
         <IoMdMail className='m-1 my-6 hover:scale-125 duration-200' />
         <MdOutlinePersonSearch className='m-1 my-6 cursor-pointer hover:scale-125 duration-200' />
         <BsFillSendFill className='m-1 my-6 hover:scale-125 duration-200'/>
-        <FaInbox className='m-1 my-6 hover:scale-125 duration-200'/>
+        <FaInbox onClick={() => onSelect('inbox')} className='m-1 my-6 hover:scale-125 duration-200'/>
         <IoIosStats className='m-1 my-6 hover:scale-125 duration-200' />
 
         </div>
